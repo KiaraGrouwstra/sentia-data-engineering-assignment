7c276e6f427004a25f730aba0f2c6ca76fab7ad4
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2021-03-01 22:50:36 +0100

    add Analysis Services

---
+++ modules/ssas.tf
@@ -0,0 +1,17 @@
+resource "azurerm_analysis_services_server" "server" {
+  name                      = "${local.prefix}-ssas"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  sku                       = "S0" # D1, B1, B2, S0, S1, S2, S4, S8, S9, S8v2, S9v2
+  admin_users               = var.emails
+  enable_power_bi_service   = true
+  querypool_connection_mode = "All" # All, ReadOnly
+  backup_blob_container_uri = azurerm_storage_account.sa.primary_blob_endpoint
+  tags                      = local.default_tags
+
+  # ipv4_firewall_rule {
+  #   name        = "myRule1"
+  #   range_start = "210.117.252.0"
+  #   range_end   = "210.117.252.255"
+  # }
+}
