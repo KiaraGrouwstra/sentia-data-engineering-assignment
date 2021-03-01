61359f4d3bea075c71a6989a8ace396b8b2851bd
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2021-03-01 22:15:41 +0100

    input variables

--- variables.tf
+++ variables.tf
@@ -23,6 +23,13 @@ variable "azure_client_secret" {
   # ^ mark the client secret as sensitive to prevent it from accidentally showing up in logs
 }
 
+variable "azure_ad_admin" {
+  description = "login username of the Azure Active Directory administrator account"
+  type        = string
+  sensitive   = true # just in case
+  default     = "AzureAD Admin"
+}
+
 # https://azure.microsoft.com/en-us/global-infrastructure/geographies/#choose-your-region
 variable "azurerm_region" {
   description = "Azure region"
@@ -35,4 +42,27 @@ variable "environment" {
   description = "name of the environment"
   type        = string
   default     = "dev"
+}
+
+variable "emails" {
+  description = "a list of email addresses to send notifications to"
+  type        = list(string)
+  default     = []
+}
+
+variable "mssql_password" {
+  description = "MS SQL Server password"
+  type        = string
+  sensitive   = true
+}
+
+variable "mysql_password" {
+  description = "MySQL Server password"
+  type        = string
+  sensitive   = true
+}
+
+variable "mongo_shard_key" {
+  description = "The name of the key to partition MongoDB on for sharding. There must not be any other unique index keys."
+  type        = string
 }
