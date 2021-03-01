04abdf44cd6853faff5a816a29cc60fe8e391f1e
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2021-03-01 17:08:57 +0100

    no public network access

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ terraform {
       source  = "hashicorp/azuread"
       version = "~> 0.7"
     }
+    powerbi = {
+      source  = "codecutout/powerbi"
+      version = "~> 1.3"
+    }
   }
 }
 
@@ -72,4 +76,68 @@ resource "random_password" "passwords" {
   count   = length(local.data_engineers)
   length  = 16
   special = true
+}
+
+# network
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local.prefix}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.default_tags
+}
+
+resource "azurerm_network_ddos_protection_plan" "ddos" {
+  name                = "${local.prefix}-ddos"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.default_tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.prefix}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  ddos_protection_plan {
+    id     = azurerm_network_ddos_protection_plan.ddos.id
+    enable = true
+  }
+  # enable VM protection for all the subnets in this Virtual Network
+  vm_protection_enabled = true
+  tags                  = local.default_tags
+}
+
+resource "azurerm_network_security_rule" "nsr_out" {
+  name                        = "${local.prefix}-nsr_out"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  description                 = "Allow outbound traffic"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  # source_application_security_group_ids = []
+  # destination_application_security_group_ids = []
+}
+
+resource "azurerm_network_security_rule" "nsr_in" {
+  name                        = "${local.prefix}-nsr_in"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  description                 = "Allow inbound traffic within the virtual network"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "*"
+  # source_application_security_group_ids = []
+  # destination_application_security_group_ids = []
 }
